13a40c9f77615a0a45e02e568c9119063178744e
Ubutu <huzaifazafar7@gmail.com>
2020-11-24 04:46:23 +0500

    new

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php 
+
+
+
+$a= ["abc","hp","dell","salsoft"];
+foreach ($a as $key => $value) {
+	echo $key.$value."<br>";
+}
+
+
+
+// $a= ["abc","hp","dell","salsoft"];
+// $b= count($a);
+// for ($i=0; $i < $b; $i++) { 
+// 	echo ($a[$i])."<br>";
+// }
+
+
+
+
+
+// $a = 6;
+// echo ($a%2 == 0 ? 'the number is even':'the number is odd') ;
+
+
+// $a = 12;
+// $x = $a % 2;
+// while($x == 0) {echo "The number is even: $a <br>";  break;} 
+// while($x == 1) {echo "The number is odd: $a <br>";  break;}  
+
+
+
+// $a=["this is even", "this is odd"];
+// $b=5;
+// echo ($a[$b % 2]);
+
+
+
+
+// $a = 5;
+// $b = $a/2;
+// settype($b, int);
+// $c = $a/$b;
+
+// // echo(gettype($b));
+// // echo($b);
+
+
+// if ($c == 2) {
+// 	echo $a."  Is even number";
+// }
+// else{
+// 	echo $a."  is odd number";
+// }
+
+
+
+
+
+
+// $name = "M. Huzaifa";
+// $age = "23";
+// $company = "salsoft";
+// echo "<p>My name is " . $name."My age is ".$age." and i work at".$company."</p>";
+
+ ?>
